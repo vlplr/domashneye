9782b4e1c3b89dfa1b2deb83dcd181d7a400921f
vlplr <vladyslavpoplor@gmail.com>
2025-12-18 23:02:00 +0100

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,9 @@
+
+function myFunc() {
+    let firstPrompt = prompt ('Напиши что-то')
+    let secondPrompt = prompt ('Напиши ешё что-то')
+    let thirdPrompt = prompt ('Напиши что-то в последний раз')
+    console.log(`${secondPrompt} ${thirdPrompt} ${firstPrompt}`);
+};
+
+myFunc();
