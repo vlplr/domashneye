584e22e8b99d179f96ccb6ae4787c33bd317e90b
vlplr <vladyslavpoplor@gmail.com>
2025-12-29 20:07:01 +0100

    Add files via upload

---
+++ dz4_2.js
@@ -0,0 +1,19 @@
+function identityCheck () {
+    let threeDigitNumber = prompt("Напиши трёхзначное число")
+    if (threeDigitNumber.length === 3 && !isNaN(threeDigitNumber)) {
+    if (threeDigitNumber[0] === threeDigitNumber[1] && threeDigitNumber[1] === threeDigitNumber[2]) {
+        console.log("Все цифры числа одинаковые")
+    } else {
+        console.log("Все цифры числа не одинаковые")
+    }
+    if (threeDigitNumber[0] === threeDigitNumber[1] || threeDigitNumber[1] === threeDigitNumber[2] || threeDigitNumber[0] === threeDigitNumber[2]) {
+        console.log("Среди цифр числа есть одинаковые")
+    } else {
+        console.log("Среди цифр числа нет одинаковых")
+    }
+    } else {
+        console.log("Это не трёхзначное число")
+    }
+}
+
+identityCheck ()
