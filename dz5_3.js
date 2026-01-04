0b2b719189e256563275faae1ed6899c7c699923
vlplr <vladyslavpoplor@gmail.com>
2026-01-04 19:02:32 +0100

    Add files via upload

---
+++ dz5_3.js
@@ -0,0 +1,8 @@
+let N = +prompt("Введи максимальный квадрат числа")
+for (let i = 1; i <= 100; i++) {
+    let squaredNumber = i * i;
+    console.log(squaredNumber);
+    if (squaredNumber > N) {
+        break;
+    }
+}
