fba6335b4e1514d12e0766f68958cf1f8f8bdb5e
vlplr <vladyslavpoplor@gmail.com>
2026-01-05 16:05:32 +0100

    Refactor loop to log squares conditionally

--- dz5_3.js
+++ dz5_3.js
@@ -1,8 +1,9 @@
 let N = +prompt("Введи максимальный квадрат числа");
 for (let i = 1; i <= 100; i++) {
     let squaredNumber = i * i;
-    console.log(squaredNumber);
+    
     if (squaredNumber > N) {
         break;
     }
+    console.log(squaredNumber);
 }
